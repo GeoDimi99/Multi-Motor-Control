527c88c6922883c7eb493b852b391f51178b7319
GeoDimi99 <dimitrov.georgi.t@gmail.com>
2022-09-15 09:20:23 +0200

    add: docs

--- PID_Motor/encoder_reader.c
+++ PID_Motor/encoder_reader.c
@@ -3,16 +3,24 @@
 #include "librarys/uart.h"  //Questo inizializza la seriale per supportare la printf
 #include <avr/io.h>
 #include "librarys/encoder.h"
+#include "librarys/pwm.h"
 
 
 int main(void){
 	//Inizializzo i oggetti utili
 	printf_init();
 	encoder_init();
+	PWM_init();
+	
+	int intesity = 100;
+	PWM_setDutyCycle(intesity);
 	
 	while(1){
-		printf("Valore encoder: %d\n",encoder_read());
-		_delay_ms(100);
+		printf("Valore encoder: %d, intensita: %d\n",encoder_read(), intesity);
+		_delay_ms(3000);
+		intesity+= 8;
+		if (intesity > 255) intesity = 0;
+		PWM_setDutyCycle(intesity);
 	}
 	
 }
--- PID_Motor/librarys/pwm.c
+++ PID_Motor/librarys/pwm.c
@@ -9,12 +9,13 @@
 // Configuration bits for PWM
 // fast PWM, 8 bit, non inverted
 // output compare set low
-#define TCCRA_MASK  (1<<WGM10) | (1<<COM1C0) | (1<<COM1C1)
+
+#define TCCRA_MASK  (1<<WGM10) | (1<<COM1C0) | (1<<COM1C1)    
 #define TCCRB_MASK ((1<<WGM12) | (1<<CS10))
-#define PORTB_MASK 1 << 7
+#define PORTB_MASK 1 << 7									//Pin 13 sulla scheda Arduino
 
 void PWM_init(void) {
-    // Use timer 1
+    // Usiamo il timer 1 per generare l'onda quadra
     TCCR1A = TCCRA_MASK;
     TCCR1B = TCCRB_MASK;
 

86bf72b30583a4e1b0d559d943baff0ed02c1cf8
sarattiani <attiani.1893420@studenti.uniroma1.it>
2022-09-23 14:33:27 +0200

    first attemp multiple slave

--- twi/master.c
+++ twi/master.c
@@ -21,8 +21,10 @@ int main(void){
 	uint8_t send_velocity[velocity_len + 1];
 	
 	uint8_t sample[] = {0x00, 's', 'a', 'm', 'p', 'l', 'e', '\0'};     //sample in broadcast
-	uint8_t get[] = {(0x01 << 1) | 0x00, 'g', 'e', 't','\0'};          //get in unicast
-	uint8_t set[] = {(0x01 << 1) | 0x00, 's', 'e', 't','\0'};          //set in unicast
+	uint8_t get_slave1[] = {(0x01 << 1) | 0x00, 'g', 'e', 't','\0'};          //get in unicast
+	uint8_t get_slave2[] = {(0x02 << 1) | 0x00, 'g', 'e', 't','\0'};          //get in unicast
+	uint8_t set_slave1[] = {(0x01 << 1) | 0x00, 's', 'e', 't','\0'};          //set in unicast
+	uint8_t set_slave2[] = {(0x02 << 1) | 0x00, 's', 'e', 't','\0'};          //set in unicast
 	uint8_t apply[] = {0x00, 'a', 'p', 'p', 'l', 'y', '\0'};           //apply in broadcast
 	send_velocity[0]= (0x01 << 1) | 0x00;
 	
@@ -31,18 +33,37 @@ int main(void){
 	do{
 		
 		TWI_Transmit_Data(sample, 8, 0);               //invio comando "sample"  
+		
+		////////////////////// COMANDI GET E SET SLAVE 1 /////////////////////////////////////////////////////////////////////////////////
 		                
-		TWI_Transmit_Data(get, 5, 0);                  //invio comando "get"
+		TWI_Transmit_Data(get_slave1, 5, 0);                  //invio comando "get"
 		
 		TWI_Read_Data(0x01, velocity_len, 0);          //leggo la velocità corrente
 		 
 		strcpy(current_velocity, Receive_Buffer);      //salvo la velocità corrente
 				              
-		TWI_Transmit_Data(set, 5, 0);                  //invio il comando "set"    
+		TWI_Transmit_Data(set_slave1, 5, 0);                  //invio il comando "set"    
 		
 		strcpy(send_velocity + 1, desired_velocity);       //inserisco la velocità desiderata dentro al buffer da inviare (indirizzo + velocità desiderata)
 		
-		TWI_Transmit_Data(send_velocity, velocity_len+1, 0);   //invio la velocità desiderata      
+		TWI_Transmit_Data(send_velocity, velocity_len+1, 0);   //invio la velocità desiderata  
+		
+		///////////////////////////COMANDI GET E SET SLAVE 2    //////////////////////////////////////////////////////////////////////////////////    
+		
+		TWI_Transmit_Data(get_slave2, 5, 0);                  //invio comando "get"
+		
+		TWI_Read_Data(0x02, velocity_len, 0);          //leggo la velocità corrente
+		 
+		strcpy(current_velocity, Receive_Buffer);      //salvo la velocità corrente
+				              
+		TWI_Transmit_Data(set_slave2, 5, 0);                  //invio il comando "set"    
+		
+		strcpy(send_velocity + 1, desired_velocity);       //inserisco la velocità desiderata dentro al buffer da inviare (indirizzo + velocità desiderata)
+		
+		TWI_Transmit_Data(send_velocity, velocity_len+1, 0);   //invio la velocità desiderata  
+		
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 	
 		TWI_Transmit_Data(apply, 7, 0);                   //invio il comando "apply"
 		
